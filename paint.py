97d83a7b46ec573312c40835c28e11434497a6b1
xueyu <xueuy@qq.com>
2023-03-06 00:23:06 +0800

    能缩放，但是不支持鼠标滚轮或者按键缩放，不好用

--- paint.py
+++ paint.py
@@ -22,8 +22,17 @@ df = pd.DataFrame(result, columns=['trade_date', 'Open', 'Close', 'High', 'Low',
 df['trade_date'] = pd.to_datetime(df['trade_date'])
 df.set_index('trade_date', inplace=True)
 
-# 绘制K线图
-mpf.plot(df, type='candle', volume=True, show_nontrading=False)
+# 创建20日和60日均线附加图表
+ma20 = df['Close'].rolling(window=20).mean()
+ma60 = df['Close'].rolling(window=60).mean()
+apds = [mpf.make_addplot(ma20, color='b'), mpf.make_addplot(ma60, color='r')]
+
+# 绘制K线图和附加图表
+mpf.plot(df, type='candle', volume=True, show_nontrading=False, addplot=apds, ylabel='Price')
+
+# 调整y轴范围以查看日K具体形态
+ylim_bottom, ylim_top = plt.ylim()
+plt.ylim(ylim_bottom - (ylim_top - ylim_bottom) * 0.1, ylim_top)
 
 # 显示K线图
 mpf.show()
