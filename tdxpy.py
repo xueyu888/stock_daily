b720b7d77bad48535a1ded83cd490c2e831c5975
xueyu <xueuy@qq.com>
2023-03-06 22:25:03 +0800

    v6

--- tdxpy.py
+++ tdxpy.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import pymysql
 from sqlalchemy import create_engine, text
 import time
@@ -14,51 +15,44 @@ start = time.time()
 # 连接 MySQL 数据库
 engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', pool_size=2000)
 
-# 从数据库中获取股票列表
-sql = "SELECT DISTINCT ts_code FROM daily_price"
+# 从数据库中获取创业板股票列表
+sql = "SELECT DISTINCT ts_code FROM daily_price WHERE ts_code LIKE '300%'"
 stocks = pd.DataFrame(engine.connect().execute(text(sql)), columns=['ts_code'])['ts_code'].tolist()
 
 # 遍历每个股票
 selected_stocks = []
 for ts_code in stocks:
     # 从数据库中获取指定股票代码的历史K线数据
-    sql = f"SELECT trade_date, open_price, close_price, high, low, vol FROM daily_price WHERE ts_code='{ts_code}' ORDER BY trade_date DESC LIMIT 50"
+    sql = f"SELECT trade_date, open_price, close_price, high, low, vol FROM daily_price WHERE ts_code='{ts_code}' ORDER BY trade_date ASC LIMIT 50"
     df = pd.DataFrame(engine.connect().execute(text(sql)), columns=['trade_date', 'open_price', 'close_price', 'high', 'low', 'vol'])
 
-    # 计算收盘价是否在五日均线之上
-    df['ma5'] = df['close_price'].rolling(5).mean()
-    above_ma5 = df['close_price'] > df['ma5']
+    # 计算MA5
+    if len(df) >= 5:
+        ma5 = df['close_price'].rolling(5).mean().shift(1)
+        df['MA5'] = ma5
 
-    # 计算5日涨幅是否大于25%
-    pct_change = df['close_price'].pct_change(5)
-    up_trend = (pct_change > 0.25) & above_ma5.shift(-4).fillna(False)
+        # 状态机
+        status = 'start'
+        last_rise = -5
+        for i, row in df.iterrows():
+            if status == 'start':
+                if row['close_price'] > row['MA5']:
+                    temp_df = df.iloc[i-4:i+1]  # 取连续的5天数据
+                    gain = temp_df['close_price'].diff().iloc[1:].sum() / temp_df['close_price'].iloc[4]  # 使用价差法计算涨幅
+                    if gain > 0.25:  # 涨幅大于25%
+                        status = 'rising'
+                        last_rise = i
+            elif status == 'rising':
+                if row['close_price'] < row['MA5']:
+                    temp_df = df.iloc[last_rise:i+1]  # 取连续的N天数据
+                    gain = temp_df['close_price'].diff().iloc[1:].sum() / temp_df['close_price'].iloc[0]  # 使用价差法计算涨幅
+                    if gain < -0.1:  # 跌幅大于10%
+                        status = 'start'
+                        selected_stocks.append(ts_code)
+            elif status in ['start', 'hold'] and row['close_price'] > row['MA5']:
+                if last_cross is None or last_cross < i - 5:
+                    if row['close_price'] > df.loc[i-1, 'MA5']:
+                        status = 'buy'
+                        last_cross = i
 
-    # 计算第二波下跌是否符合条件
-    below_ma5 = df['close_price'] < df['ma5']
-    down_trend = below_ma5 & (below_ma5.shift(1) | below_ma5.shift(2) | below_ma5.shift(3))
-    continuous_drop_days = down_trend.rolling(7, min_periods=4).sum()
-
-    # 计算上穿五日线是否符合条件
-    above_ma5_again = df['close_price'] > df['ma5']
-    cross_above_ma5 = above_ma5_again & (above_ma5_again.shift(1) | above_ma5_again.shift(2) | above_ma5_again.shift(3) | above_ma5_again.shift(4))
-    pct_change_again = df['close_price'].pct_change().fillna(0)
-    close_gain = pct_change_again[above_ma5_again & cross_above_ma5]
-    
-    # 满足条件1和2
-    if up_trend.any() and (continuous_drop_days >= 4).any():
-        # 找到满足条件1和2的第一个位置
-        start = up_trend[up_trend].index[-1]
-
-        # 判断是否满足条件3
-        for end in continuous_drop_days[continuous_drop_days >= 4].index:
-            if end < start:
-                continue
-            if (close_gain.loc[start:end] >= 0.02).any() and (close_gain.loc[start:end] < 0.06).all():
-                selected_stocks.append(ts_code)
-                break
-
-# 输出符合条件的股票代码
 print(selected_stocks)
-
-# 关闭数据库连接
-engine.dispose()
