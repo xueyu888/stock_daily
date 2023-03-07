4c131c12250c250866e9ad4299a2ab44e58319a4
xueyu <xueuy@qq.com>
2023-03-07 23:48:26 +0800

    v7 有点意思了，可以选出一些票来了，还需要修改一下。

--- tdxpy.py
+++ tdxpy.py
@@ -23,17 +23,19 @@ stocks = pd.DataFrame(engine.connect().execute(text(sql)), columns=['ts_code'])[
 selected_stocks = []
 for ts_code in stocks:
     # 从数据库中获取指定股票代码的历史K线数据
-    sql = f"SELECT trade_date, open_price, close_price, high, low, vol FROM daily_price WHERE ts_code='{ts_code}' ORDER BY trade_date ASC LIMIT 50"
+    sql = f"SELECT trade_date, open_price, close_price, high, low, vol FROM daily_price WHERE ts_code='{ts_code}' ORDER BY trade_date DESC LIMIT 50"
     df = pd.DataFrame(engine.connect().execute(text(sql)), columns=['trade_date', 'open_price', 'close_price', 'high', 'low', 'vol'])
+    df = df.sort_values(by='trade_date', ascending=True).reset_index(drop=True)
 
     # 计算MA5
     if len(df) >= 5:
-        ma5 = df['close_price'].rolling(5).mean().shift(1)
+        ma5 = df['close_price'].rolling(5).mean()
         df['MA5'] = ma5
 
-        # 状态机
+             # 状态机
         status = 'start'
         last_rise = -5
+        consecutive_below_MA5 = 0
         for i, row in df.iterrows():
             if status == 'start':
                 if row['close_price'] > row['MA5']:
@@ -47,12 +49,16 @@ for ts_code in stocks:
                     temp_df = df.iloc[last_rise:i+1]  # 取连续的N天数据
                     gain = temp_df['close_price'].diff().iloc[1:].sum() / temp_df['close_price'].iloc[0]  # 使用价差法计算涨幅
                     if gain < -0.1:  # 跌幅大于10%
-                        status = 'start'
-                        selected_stocks.append(ts_code)
-            elif status in ['start', 'hold'] and row['close_price'] > row['MA5']:
-                if last_cross is None or last_cross < i - 5:
+                        status = 'falling'
+            elif status == 'falling':
+                if row['close_price'] < row['MA5']:
+                    consecutive_below_MA5 += 1
+                    if consecutive_below_MA5 >= 4 and consecutive_below_MA5 <= 7 and all(df.iloc[i-consecutive_below_MA5+1:i+1]['close_price'] < df.iloc[i-consecutive_below_MA5+1:i+1]['MA5']):
+                        status = 'waiting'
+            elif status == 'waiting' and row['close_price'] > row['MA5']:
                     if row['close_price'] > df.loc[i-1, 'MA5']:
                         status = 'buy'
-                        last_cross = i
+                        selected_stocks.append(ts_code)
+
 
 print(selected_stocks)
