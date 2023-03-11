409dfff4c5ff30d5b306f5a81efd5ed6740bff88
xueyu <xueuy@qq.com>
2023-03-11 10:26:39 +0800

    v12

--- tdxpy.py
+++ tdxpy.py
@@ -23,23 +23,20 @@ pool = QueuePool(
 # 创建数据库连接
 engine = create_engine('mysql+pymysql://', pool=pool)
 
-
 start = time.time()
 
-# 连接 MySQL 数据库
-#engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', pool_size=40)
-
 # 从数据库中获取创业板股票列表
-sql = "SELECT DISTINCT ts_code FROM daily_price "
+sql = "SELECT DISTINCT ts_code FROM daily_price WHERE ts_code NOT LIKE '83%' AND ts_code NOT LIKE '688%'"
 stocks = pd.DataFrame(engine.connect().execute(text(sql)), columns=['ts_code'])['ts_code'].tolist()
 
 # 遍历每个股票
 selected_stocks = [{'date': '', 'stock': ''}]
-returns = [] # 创建一个空列表用于存储收益率
+returns = []  # 创建一个空列表用于存储收益率
 for ts_code in stocks:
     # 从数据库中获取指定股票代码的历史K线数据
     sql = f"SELECT trade_date, ts_code, open_price, close_price, high, low, vol FROM daily_price WHERE ts_code='{ts_code}' ORDER BY trade_date DESC LIMIT 50"
-    df = pd.DataFrame(engine.connect().execute(text(sql)), columns=['trade_date','ts_code', 'open_price', 'close_price', 'high', 'low', 'vol'])
+    df = pd.DataFrame(engine.connect().execute(text(sql)), columns=[
+                      'trade_date', 'ts_code', 'open_price', 'close_price', 'high', 'low', 'vol'])
     df = df.sort_values(by='trade_date', ascending=True).reset_index(drop=True)
     # 计算MA5
     if len(df) >= 5:
@@ -52,50 +49,59 @@ for ts_code in stocks:
         consecutive_below_MA5 = 0
         for i, row in df.iterrows():
             if status == 'start':
-                    if row['close_price'] > row['MA5']:
-                            if i+5 >= len(df):
-                                break  # 数据不足6天，退出循环
-                            temp_df = df.loc[i:i+5]  # 取连续的6天数据
-                            gain = (temp_df['close_price'].iloc[5] - temp_df['close_price'].iloc[0]) / temp_df['close_price'].iloc[0]
-                            if gain > 0.25:  # 涨幅大于25%
-                                status = 'rising'
-                                last_rise = i
+                if row['close_price'] > row['MA5']:
+                    if i+5 >= len(df):
+                        break  # 数据不足6天，退出循环
+                    temp_df = df.loc[i:i+5]  # 取连续的6天数据
+                    gain = (temp_df['close_price'].iloc[5] -
+                            temp_df['close_price'].iloc[0]) / temp_df['close_price'].iloc[0]
+                    if gain > 0.20:  # 涨幅大于25%
+                        status = 'rising'
+                        last_rise = i
             elif status == 'rising':
                 if row['close_price'] < row['MA5']:
-                        if i+4 >= len(df):
-                            break # 数据不足五天，退出循环
-                        next_five_days_df = df.loc[i:i+4] # 取接下来的五天数据，包括首次跌破的那一天
-                        if next_five_days_df['close_price'].lt(next_five_days_df['MA5']).all(): # 如果接下来的五天收盘价都在五日线之下
-                            status = 'falling'
-                        else: # 否则回到start状态，重新寻找涨幅大于25%的情况
-                            status = 'start'
+                    if i+2 >= len(df):
+                        break  # 数据不足三天，退出循环
+                    next_three_days_df = df.loc[i:i+2]  # 取接下来的五天数据，包括首次跌破的那一天
+                    # 如果接下来的五天收盘价都在五日线之下
+                    if next_three_days_df['close_price'].lt(next_three_days_df['MA5']).all():
+                        status = 'falling'
+                    else:  # 否则回到start状态，重新寻找涨幅大于25%的情况
+                        status = 'start'
 
             elif status == 'falling' and row['close_price'] > row['MA5']:
-                    if row['close_price'] > df.loc[i-1, 'MA5']:
-                        status = 'buy'
-                        selected_stocks.append({'date': row['trade_date'], 'stock': ts_code}) # 添加买入信号到列表
+                if row['close_price'] > df.loc[i-1, 'MA5']:
+                    status = 'buy'
+                    selected_stocks.append(
+                        {'date': row['trade_date'], 'stock': ts_code})  # 添加买入信号到列表
 
-                        # 遍历每个买入信号，计算收益率并添加到列表中                        
-                        for signal in selected_stocks:
-                            date_buy = signal['date']
-                            stock_buy = signal['stock']
-                            buy_row = df.loc[(df.ts_code == stock_buy) & (df.trade_date == date_buy)]
-                            
-                            if buy_row.empty:
-                                continue
-                                
-                            buy_price = float(buy_row['close_price']) # 获取买入价格
-                            
-                            date_sell_index= int(buy_row.index.values) + 1 
-                            sell_row= df.iloc[date_sell_index]
-                            
-                            sell_stock= sell_row.ts_code 
-                            sell_date= sell_row.trade_date 
-                            sell_close= float(sell_row.close_price)
-                            
-                            return_rate= (sell_close - buy_price )/buy_price 
-                            
-                            returns.append({'date': row['trade_date'], 'stock': ts_code, 'return_rate':return_rate})
+                    # 遍历每个买入信号，计算收益率并添加到列表中
+                    for signal in selected_stocks:
+                        date_buy = signal['date']
+                        stock_buy = signal['stock']
+                        buy_row = df.loc[(df.ts_code == stock_buy) & (
+                            df.trade_date == date_buy)]
+
+                        if buy_row.empty:
+                            continue
+
+                        buy_price = float(buy_row['close_price'])  # 获取买入价格
+
+                        date_sell_index = int(buy_row.index.values) + 1
+                        # 确保 date_sell_index 小于等于 df 的最大索引值
+                        if date_sell_index <= df.index.max():
+                            sell_row = df.iloc[date_sell_index]
+                        else:
+                            # 如果 date_sell_index 超出了 df 的最大索引值，则将 sell_row 设为 None 或其他值
+                            continue
+
+                        sell_stock = sell_row.ts_code
+                        sell_date = sell_row.trade_date
+                        sell_close = float(sell_row.close_price)
+
+                        return_rate = (sell_close - buy_price)/buy_price
+
+                        returns.append({'date': row['trade_date'], 'stock': ts_code, 'return_rate': return_rate})
 
 
 # 将数据写入CSV文件
@@ -105,11 +111,12 @@ with open('returns.csv', mode='w', newline='') as csv_file:
 
     writer.writeheader()
     for item in returns:
-        writer.writerow({'date': item['date'], 'stock': item['stock'], 'return_rate': "{:.2%}".format(item['return_rate'])})
+        writer.writerow(
+            {'date': item['date'], 'stock': item['stock'], 'return_rate': item['return_rate']})
 
-# 计算平均收益率并打印结果    
-average_return_rate= np.mean(returns.returns)
-                        
+# 计算平均收益率并打印结果
+average_return_rate = np.mean(returns.return_rate)
+
 print(selected_stocks)
 end = time.time()
-print(end -start)
+print(end - start)
